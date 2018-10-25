c2b65240425b1817ded979827797aed0fcae2025
Pio Molina <vpio@comcast.net>
2018-10-25 00:21:10 -0400

    added cards that dont look good

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import SimpleCard from './SimpleCard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Pio
+        <div>
+          <SimpleCard />
+          <SimpleCard />
+        </div>
       </div>
     );
   }
