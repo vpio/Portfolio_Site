dfbc43a712e2180f31a3f837096a49e5472d66ef
Pio Molina <vpio@comcast.net>
2018-10-25 00:37:50 -0400

    added grid component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import SimpleCard from './SimpleCard';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Pio
-        <div>
-          <SimpleCard />
-          <SimpleCard />
-        </div>
+        <CssBaseline>
+          Pio
+          <div>
+            <Grid container spacing={16}>
+              <Grid item xs={12}>
+                <SimpleCard />
+              </Grid>
+              <Grid item xs={12}>
+                <SimpleCard />
+              </Grid>
+              <Grid item xs={12}>
+                <SimpleCard />
+              </Grid>
+            </Grid>
+          </div>
+        </CssBaseline>
       </div>
     );
   }
